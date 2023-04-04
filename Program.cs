a4895e788c6dec2936a7616b46ac3946e9453e0e
Mimi <mireiag77@gmail>
2023-04-04 14:27:13 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_Secuencial_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            Console.WriteLine("La suma es: {0}", num1 + num2);
+        }
+    }
+}
